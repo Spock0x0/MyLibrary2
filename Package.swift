60cc99369dc429c4779487758616a62984bb340c
Spock <spock.hsueh@kkday.com>
2022-10-26 11:25:34 +0800

    add package 3

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "MyLibrary3", url: "https://github.com/Spock0x0/MyLibrary3.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary2",
-            dependencies: [],
+            dependencies: ["MyLibrary3"],
             path: "Sources"
         ),
         .testTarget(
--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -1,5 +1,9 @@
+import MyLibrary3
+
 public struct MyLibrary2 {
-    public private(set) var text = "Hello, World!"
+    public private(set) var text = "Hello, World 2"
+    
+    public private(set) var text2 = MyLibrary3().text
 
     public init() {
     }
